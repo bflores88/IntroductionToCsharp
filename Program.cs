4691625ea525a7af8a9e27e387dab38ba392b9b0
kmietotwy <kmietotwy@gmail.com>
2019-09-02 12:03:12 -0700

    built-in type: double

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+//        Console.WriteLine(("Please enter your first name"));
+//        string firstName = Console.ReadLine();
+//        
+//        Console.WriteLine(("Please enter your last name"));
+//        string lastName = Console.ReadLine();
+//        
+////        Concatenation Method
+////        Console.WriteLine("Hello " + userName);
+//
+////        Placeholder Syntax (Preferred)
+//        Console.WriteLine("Hello {0}, {1}", firstName, lastName);
+
+//      Booleans must be true or false
+//        bool b = true;
+//
+//        b = "bird";
+
+//        int i = 00;
+//        Console.WriteLine("Min = {0}", int.MinValue);
+//        Console.WriteLine("Max = {0}", int.MaxValue);
+
+        double d = 123.24685486454;
+        Console.WriteLine(d);
+    }
+}
