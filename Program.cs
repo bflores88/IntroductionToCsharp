76fdfbaa6a699bc050a07eaab1764914928c90f1
kmietotwy <kmietotwy@gmail.com>
2019-09-03 14:55:43 -0700

    using while loop to increment: initialize, condition, increment

--- Program.cs
+++ Program.cs
@@ -5,44 +5,18 @@ class Program
 {
     static void Main()
     {
-        int totalCoffeeCost = 0;
-        string userDecision = string.Empty;
+        int[] Numbers = new int[3];
 
-        do
+        Numbers[0] = 101;
+        Numbers[1] = 102;
+        Numbers[2] = 103;
+
+        int i = 0;
+
+        while (i < Numbers.Length)
         {
-            Console.WriteLine("Please Select Your Coffee Size: 1 - Small, 2 - Medium, 3 - Large");
-            int userChoice = int.Parse(Console.ReadLine());
-
-            switch (userChoice)
-            {
-                case 1:
-                    totalCoffeeCost += 1;
-                    break;
-                case 2:
-                    totalCoffeeCost += 2;
-                    break;
-                case 3:
-                    totalCoffeeCost += 3;
-                    break;
-                default:
-                    Console.WriteLine("Your choice {0} is invalid", userChoice);
-                    break;
-            }
-
-            do
-            {
-                Console.WriteLine("Do you want to buy another coffee - Yes or No?");
-                userDecision = Console.ReadLine();
-
-                if (userDecision.ToUpper() != "YES" && userDecision.ToUpper() != "NO")
-                {
-                    Console.WriteLine("Your choice {0} is invalid.  Please try again...", userDecision);
-                }
-            } while (userDecision.ToUpper() != "YES" && userDecision.ToUpper() != "NO");
-        } while (userDecision.ToUpper() != "NO");
-
-        Console.WriteLine("Thank you for shopping with us");
-        Console.WriteLine("Bill amount = {0}", totalCoffeeCost);
-
+            Console.WriteLine(Numbers[i]);
+            i++;
+        }
     }
 }
