87231d14f0a8c16e61425b25ae010ffb93a183b9
kmietotwy <kmietotwy@gmail.com>
2019-09-03 11:06:42 -0700

    basic while loop

--- Program.cs
+++ Program.cs
@@ -5,45 +5,15 @@ class Program
 {
     static void Main()
     {
-        int totalCoffeeCost = 0;
-        
-        Start:
-       Console.WriteLine("Please Select Your Coffee Size: 1 - Small, 2 - Medium, 3 - Large");
-       int userChoice = int.Parse(Console.ReadLine());
+     Console.WriteLine("Please enter your target");
+     int userTarget = int.Parse(Console.ReadLine());
 
-       switch (userChoice)
-       {
-           case 1:
-               totalCoffeeCost += 1;
-               break;
-           case 2:
-               totalCoffeeCost += 2;
-               break;
-           case 3:
-               totalCoffeeCost += 3;
-               break;
-           default:
-               Console.WriteLine("Your choice {0} is invalid", userChoice);
-               goto Start;
-       }
-      
-       Decide:
-       Console.WriteLine("Do you want to buy another coffee - Yes or No?");
-       string userDecision = Console.ReadLine();
+     int start = 10;
 
-       switch (userDecision.ToUpper())
-       {
-           case "YES":
-               goto Start;
-           case "NO":
-               break;
-           default:
-               Console.WriteLine("Your choice is {0} invalid.  Please try again.", userDecision);
-               goto Decide;
-       }
-       
-       Console.WriteLine("Thank you for shopping with us");
-       Console.WriteLine("Bill amount = {0}", totalCoffeeCost);
-       
+     while (start <= userTarget)
+     {
+         Console.Write(start + " ");
+         start = start + 2;
+     }
     }
 }
