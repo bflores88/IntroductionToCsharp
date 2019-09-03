e060f4e65f2b467656c4f061d408ebde82c34db2
kmietotwy <kmietotwy@gmail.com>
2019-09-03 15:08:06 -0700

    examples of break and continue in for loops

--- Program.cs
+++ Program.cs
@@ -5,28 +5,19 @@ class Program
 {
     static void Main()
     {
-        int[] Numbers = new int[3];
-
-        Numbers[0] = 101;
-        Numbers[1] = 102;
-        Numbers[2] = 103;
-
-        foreach (int k in Numbers)
+        // break statement causes the control to get out of the loop
+        for (int i = 1; i <= 20; i++)
         {
-            Console.WriteLine(k);
+            Console.WriteLine(i);
+            
+            if(i==10) break;
         }
-
-        for (int j = 0; j < Numbers.Length; j++)
+        
+        // continue statement will end code execution in the for loop and start again at the next increment
+        for (int j = 0; j <= 20; j++)
         {
-            Console.WriteLine(Numbers[j]);
-        }
-
-        int i = 0;
-
-        while (i < Numbers.Length)
-        {
-            Console.WriteLine(Numbers[i]);
-            i++;
+            if(j%2 == 1) continue;
+            Console.WriteLine(j);
         }
     }
 }
