c9937dd2cf582b17fa5d1a21a2a94b015fb6607e
kmietotwy <kmietotwy@gmail.com>
2019-09-03 10:54:18 -0700

    switch statement, goto statement, and labels - sample coffee order applications

--- Program.cs
+++ Program.cs
@@ -1,23 +1,49 @@
 ﻿using System;
+using System.Text;
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Please enter a number");
+        int totalCoffeeCost = 0;
+        
+        Start:
+       Console.WriteLine("Please Select Your Coffee Size: 1 - Small, 2 - Medium, 3 - Large");
+       int userChoice = int.Parse(Console.ReadLine());
 
-        int userNumber = int.Parse(Console.ReadLine());
+       switch (userChoice)
+       {
+           case 1:
+               totalCoffeeCost += 1;
+               break;
+           case 2:
+               totalCoffeeCost += 2;
+               break;
+           case 3:
+               totalCoffeeCost += 3;
+               break;
+           default:
+               Console.WriteLine("Your choice {0} is invalid", userChoice);
+               goto Start;
+       }
+      
+       Decide:
+       Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+       string userDecision = Console.ReadLine();
 
-        switch (userNumber)
-        {
-            case 10:
-            case 20:
-            case 30:
-                Console.WriteLine("Your number is {0}", userNumber);
-                break;
-            default:
-                Console.WriteLine("Your number is not 10, 20 or 30");
-                break;
-        }
-
+       switch (userDecision.ToUpper())
+       {
+           case "YES":
+               goto Start;
+           case "NO":
+               break;
+           default:
+               Console.WriteLine("Your choice is {0} invalid.  Please try again.", userDecision);
+               goto Decide;
+       }
+       
+       Console.WriteLine("Thank you for shopping with us");
+       Console.WriteLine("Bill amount = {0}", totalCoffeeCost);
+       
     }
 }
