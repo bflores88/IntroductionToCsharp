e185b5ae2bdb5517415fcdaed25eccbe8bee5841
kmietotwy <kmietotwy@gmail.com>
2019-09-02 17:00:54 -0700

    operators

--- Program.cs
+++ Program.cs
@@ -3,15 +3,36 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        string name = "\"Brenda\"";
-        Console.WriteLine(name);
+        // Assignment operator: =
+        int i = 10;
+        bool b = true;
+        
+        // Arithmetic operators: +, -, *, /, %
+        int Numerator = 10;
+        int Denominator = 2;
+        int Result = Numerator % Denominator;
+        Console.WriteLine("Result = {0}", Result);
+        
+        // Comparison operators: ==, !=, >, >=, <, <=
+        int Number = 10;
+        if (Number == 10)
+        {
+            Console.WriteLine(true);
+        }
+        
+        // Conditional operators: &&, ||
+        int AnotherNumber = 20;
+        if (Number == 10 || AnotherNumber != 20)
+        {
+            Console.WriteLine("Hello World");
+        }
+        
+        // Ternary operator: ?
+        bool isNumber10 = Number == 10 ? true : false;
+        
+        Console.WriteLine("Number == to is {0}", isNumber10);
 
-        string nums = "One\nTwo\nThree";
-        Console.WriteLine(nums);
-
-        string path = @"C:\\brenda\\DotNet\\Training\\Csharp";
-        Console.WriteLine(path);
     }
 }
