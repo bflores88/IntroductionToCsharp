64d995ff49aaeb11363c7b49dd2d5eeab7a1885b
kmietotwy <kmietotwy@gmail.com>
2019-09-03 10:33:20 -0700

    basic switch statement

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@ class Program
 
         int userNumber = int.Parse(Console.ReadLine());
 
-        if (userNumber == 10 || userNumber == 20)
+        switch (userNumber)
         {
-            Console.WriteLine("Your number is 10 or 20");
+            case 10:
+                Console.WriteLine("Your number is 10");
+                break;
+            case 20:
+                Console.WriteLine("Your number is 20");
+                break;
+            case 30:
+                Console.WriteLine("Your number is 30");
+                break;
+            default:
+                Console.WriteLine("Your number is not 10, 20 or 30");
+                break;
         }
 
     }
