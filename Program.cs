3ba7e39599f3ba105b8445d6a9941118497ca683
kmietotwy <kmietotwy@gmail.com>
2019-09-02 17:12:43 -0700

    example use of nullable types and how to convert a value type to a nullable type

--- Program.cs
+++ Program.cs
@@ -5,34 +5,18 @@ class Program
 {
     static void Main()
     {
-        // Assignment operator: =
-        int i = 10;
-        bool b = true;
-        
-        // Arithmetic operators: +, -, *, /, %
-        int Numerator = 10;
-        int Denominator = 2;
-        int Result = Numerator % Denominator;
-        Console.WriteLine("Result = {0}", Result);
-        
-        // Comparison operators: ==, !=, >, >=, <, <=
-        int Number = 10;
-        if (Number == 10)
-        {
-            Console.WriteLine(true);
-        }
-        
-        // Conditional operators: &&, ||
-        int AnotherNumber = 20;
-        if (Number == 10 || AnotherNumber != 20)
-        {
-            Console.WriteLine("Hello World");
-        }
-        
-        // Ternary operator: ?
-        bool isNumber10 = Number == 10 ? true : false;
-        
-        Console.WriteLine("Number == to is {0}", isNumber10);
+        bool? AreYouMajor = null;
 
+        if (AreYouMajor == true)
+        {
+            Console.WriteLine("User is a Major");
+        } else if (AreYouMajor == false)
+        {
+            Console.WriteLine("User is Not Major");
+        }
+        else
+        {
+            Console.WriteLine("User did not answer the question");
+        }
     }
 }
