deccde084c59e665243841097b394203ef9474d9
kmietotwy <kmietotwy@gmail.com>
2019-09-03 15:01:12 -0700

    comparing for each to for loop and while loop: for each does not need to know how many elements are in the collection

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ class Program
         Numbers[1] = 102;
         Numbers[2] = 103;
 
+        foreach (int k in Numbers)
+        {
+            Console.WriteLine(k);
+        }
+
         for (int j = 0; j < Numbers.Length; j++)
         {
             Console.WriteLine(Numbers[j]);
