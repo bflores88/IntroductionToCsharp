ee505a88a305b44f48a2f14a3a7afad3ed97bacf
kmietotwy <kmietotwy@gmail.com>
2019-09-04 14:42:01 -0700

    namespaces: creating class libraries and nesting namespaces

--- Program.cs
+++ Program.cs
@@ -5,41 +5,9 @@ class Program
 {
     public static void Main()
     {
-        // alias directives can be used to resolve ambiguity between names (other than fully qualified name)
         PATA.ClassA.Print();
         PATB.ClassA.Print();
-        
-        // alias directives are also helpful when the namespace names are very long
     }
 
 }
 
-namespace ProjectA
-{
-    namespace TeamA
-    {
-        class ClassA
-        {
-            public static void Print()
-            {
-                Console.WriteLine("Team A Print Method");
-            }
-        }
-}
-
-}
-
-namespace ProjectA
-{
-    namespace TeamB
-    {
-        class ClassA
-        {
-            public static void Print()
-            {
-                Console.WriteLine("Team B Print Method");
-            }
-        }
-}
-
-}
