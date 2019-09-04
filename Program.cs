cb831ca96b66ea46196729340ed095fdeac59845
kmietotwy <kmietotwy@gmail.com>
2019-09-04 10:12:13 -0700

    methods: example output parameters

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ using System.Text;
 class Program
 {
     public static void Main()
-    { 
-        int i = 0;
-        SimpleMethod(ref i);
-        Console.WriteLine(i); // 0
-        
-        
-    }
-
-    public static void SimpleMethod(ref int j)
     {
-        j = 101;
+        int Total = 0;
+        int Product = 0;
+       Calculate(10, 20, out Total, out Product);
+       
+       Console.WriteLine("Sum = {0} && Product = {1}", Total, Product);
     }
+    public static void Calculate(int fN, int sN, out int Sum, out int Product)
+    {
+        Sum = fN + sN;
+        Product = fN * sN;
+    } 
 }
