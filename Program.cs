51f23ebe48df8d6f0d317699a022ee858f93e5aa
kmietotwy <kmietotwy@gmail.com>
2019-09-23 14:11:22 -0700

    initializing structs

--- Program.cs
+++ Program.cs
@@ -1,47 +1,37 @@
 ﻿using System;
 
-public class Student
+public struct Customer
 {
+    // private fields
     private int _id;
     private string _name;
-    private int _passMark = 35;
 
-    public int PassMark
-    {
-        get => this._passMark;
-    }
-
+    
+    // public properties
     public string Name
     {
-        set
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                throw new Exception("Name cannot be null or empty");
-            }
-
-            this._name = value;
-        }
-
-        get => string.IsNullOrEmpty(this._name) ? "No Name" : this._name;
+        get => this._name;
+        set => this._name = value;
     }
-
     public int Id
     {
-        set
-        {
-            
-            if (value <= 0)
-            {
-                throw new Exception("Student Id cannot be negative");
-            }
-
-            this._id = value;
-        }
-
         get => this._id;
+        set => this._id = value;
+    }
+
+    // constructor
+    public Customer(int Id, string Name)
+    {
+        this._id = Id;
+        this._name = Name;
     }
     
+    // methods
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Id = {0} && Name = {1}", this._id, this._name);
+    }
 }
 
 
@@ -49,11 +39,23 @@ public class Program
 {
     public static void Main()
     {
-        Student c1 = new Student();
-        c1.Id = 101;
-        c1.Name = "Brenda";
-        Console.WriteLine("Student Id = {0} && Student Name = {1} && Student PassMark = {2}", c1.Id, c1.Name, c1.PassMark);
-
+        // using constructor
+        Customer c1 = new Customer(101, "Mark");
+        c1.PrintDetails();
+        
+        // using default constructor and public methods
+        Customer c2 = new Customer();
+        c2.Id = 102;
+        c2.Name = "Ferdinand";
+        c2.PrintDetails();
+        
+        // using object initializer syntax
+        Customer c3 = new Customer
+        {
+            Id = 103,
+            Name = "Robert"
+        };
+        c3.PrintDetails();
     }
 
 
